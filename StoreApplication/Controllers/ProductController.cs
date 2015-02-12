94f08f93c2ad570d4e9c1556638655828434dd7b
aclel <andrew.cleland3@gmail.com>
2015-02-12 23:01:24 +1000

    Inital commit. Setup Product index page. NHibernate working.

---
+++ StoreApplication/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NHibernate;
+using NHibernate.Linq;
+using StoreApplication.Models;
+
+namespace StoreApplication.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        // GET: /Product/
+        public ActionResult Index()
+        {
+            using (ISession session = NHibernateSession.OpenSession())
+            {
+                var products = session.Query<Product>().ToList();
+                return View(products);
+            }
+        }
+    }
+}
---
+++ StoreApplication/Models/NHibernateSession.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NHibernate;
+using NHibernate.Cfg;
+
+namespace StoreApplication.Models
+{
+    public class NHibernateSession
+    {
+        /** 
+         * Opens an NHibernate session which allows communication with the database.
+         */
+        public static ISession OpenSession()
+        {
+            var configuration = new Configuration();
+            var configurationPath = HttpContext.Current.Server.MapPath(@"~\Models\NHibernate\hibernate.cfg.xml");
+            configuration.Configure(configurationPath);
+            var productConfigurationFile = HttpContext.Current.Server.MapPath(@"~\Models\NHibernate\Product.hbm.xml");
+            configuration.AddFile(productConfigurationFile);
+            ISessionFactory sessionFactory = configuration.BuildSessionFactory();
+            return sessionFactory.OpenSession();
+        }
+    }
+}
