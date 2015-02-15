740edd4991d90027a09e78299013852cfedbbf8a
aclel <andrew.cleland3@gmail.com>
2015-02-15 12:43:17 +1000

    Deployed project to Windows Azure. Application now uses Azure database.

---
+++ StoreApplication.Tests/ProductControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StoreApplication.Controllers;
+using System.Web.Mvc;
+
+namespace StoreApplication.Tests
+{
+    [TestClass]
+    public class ProductControllerTest
+    {
+        [TestMethod]
+        public void TestDetailsView()
+        {
+            var controller = new ProductController();
+            var result = controller.Details(2) as ViewResult;
+            Assert.AreEqual("Details", result.ViewName);
+        }
+
+        [TestMethod]
+        public void TestIndexView()
+        {
+            var controller = new ProductController();
+            var result = controller.Index() as ViewResult;
+            Assert.AreEqual("Index", result.ViewName);
+        }
+    }
+}
